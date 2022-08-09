2539f8c00be624583663f2b5caabcb9b5ac93903
vfunny2000@rambler.ru <vfunny2000@rambler.ru>
2022-08-09 17:00:21 +0300

    added Odd/Even

---
+++ Task006-Even-Odd/Program.cs
@@ -0,0 +1,11 @@
+﻿ Console.WriteLine("Введите число: ");
+ int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 1)
+            {
+                Console.WriteLine("Число " + num + " является: НЕЧЁТНЫМ");
+            }
+            else
+            {
+                Console.WriteLine("Число " + num + " является: ЧЁТНЫМ");
+            }
