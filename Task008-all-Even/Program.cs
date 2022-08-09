09e7707fcdaa833af5f55c7349ee53667387fc04
vfunny2000@rambler.ru <vfunny2000@rambler.ru>
2022-08-09 17:23:47 +0300

    added all even from 1 to N

---
+++ Task008-all-Even/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Enter your number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int res = 1;
+
+if (res == 1)
+
+{         
+    res += 1;
+}
+
+while (res <= num)
+
+{
+    Console.WriteLine(res);
+    res += 2;
+}      
+    
+Console.Write("Here you are-)");
