2cfb3704bb0fe9d3f7dd9c76579940485f48bfc9
vfunny2000@rambler.ru <vfunny2000@rambler.ru>
2022-08-09 16:49:22 +0300

    added max of 3

---
+++ Task004-max-of-3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите 3 числа:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            int max = a;
+
+            if (b > max)
+            {
+                max = b;
+            }
+
+            if (c > max)
+            {
+                max = c;
+            }
+
+            Console.WriteLine("Наибольшее из введённых чисел -> " + max);
